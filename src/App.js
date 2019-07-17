87e210fc226fbbaf7452b04509ffd2a78b3cd959
Sitesh Pattanaik <sitesh.pattanaik@acko.tech>
2019-07-17 22:10:13 +0530

    Created LearningPathCreate component

--- src/App.js
+++ src/App.js
@@ -3,15 +3,17 @@ import { BrowserRouter as Router, Route} from 'react-router-dom'
 import Home from './Home.js'
 import LoginPage from './containers/Login/LoginPage.js'
 import RegisterPage from './containers/Register/RegisterPage.js'
+import CreateLearningPath from './containers/LearningPath/CreateLearningPath.js';
 
 
 function App() {
   return (
       <Router>
     <div className="App">
-      <Route exact path="/" component={Home} />
+      {/* <Route exact path="/" component={Home} />
       <Route path="/login" component={LoginPage} />
-      <Route path="/register" component={RegisterPage} />
+      <Route path="/register" component={RegisterPage} /> */}
+      <CreateLearningPath />
     </div>
       </Router>
   );
